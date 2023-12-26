e79107dc9a2041cf74f7cd94171545565c55703b
neominalsolutions <neominal.solutions@gmail.com>
2023-12-26 11:28:22 +0300

    UseEffect useState

--- src/demo/useEffect/useEffectSample.tsx
+++ src/demo/useEffect/useEffectSample.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function useEffectSample() {
-	return <div>useEffectSample</div>;
+	const [random, setRandom] = useState<number | undefined>();
+
+	useEffect(() => {
+		// componentdidmount => componentin doma ilk basıldığı an
+		// ilk load aşamasında componente veri çekme işlemleri burada yapılır
+		console.log('useEffect Load');
+		console.log('guncel state rdm', random);
+	}, []); // [] herhangi bir state takio etmiyorum. sadece ilk açılışta çalışırım
+
+	useEffect(() => {
+		if (random !== undefined) {
+			// 2.kullanım durumu state takibi
+			console.log('random state değişimin yakalarız');
+			// state değişimi sonrasında veri çekmek amaçlı kullanılabilir.
+		}
+	}, [random]); // [state] // random değişiminde tatiklenirim.
+
+	return (
+		<div>
+			<p>Random : {random}</p>
+			<button
+				onClick={() => {
+					const rdm = Math.random() * 100;
+					setRandom(rdm);
+				}}
+			>
+				Random
+			</button>
+		</div>
+	);
 }
 
 export default useEffectSample;
--- src/demo/useState/useStateSample.tsx
+++ src/demo/useState/useStateSample.tsx
@@ -1,9 +1,108 @@
 //rfce
 
-import React from 'react';
+import { Component, useState } from 'react';
+
+// import React, { useState } from 'react';
+
+// import React, { Component } from 'react';
+
+type PropsSample = {
+	counter?: number;
+};
+
+type CounterState = {
+	counter: number;
+};
+
+// export default class useStateSample extends Component<
+// 	PropsSample,
+// 	CounterState
+// > {
+// 	state: CounterState = { counter: 0 };
+
+// 	constructor(props: PropsSample) {
+// 		super(props);
+// 		this.state.counter = props.counter || 1;
+// 		this.increase = this.increase.bind(this);
+// 	}
+// 	// method;
+// 	increase() {
+// 		this.setState({ counter: this.state.counter + 1 }, () => {
+// 			console.log('state güncellendiği an');
+// 		});
+// 	}
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				{this.state.counter}
+
+// 				<button onClick={this.increase}>(+)</button>
+// 			</div>
+// 		);
+// 	}
+// }
+
+interface Person {
+	firstName: string; // required
+	lastName: string;
+	address?: Address; // optional
+	friends: Friend[];
+}
+
+interface Address {
+	city: string;
+	country: string;
+}
+
+interface Friend {
+	fullName: string;
+	age: number;
+}
 
 function useStateSample() {
-	return <div>useStateSample</div>;
+	const [counter, setCounter] = useState<number>(0); // value type state
+	const [person, setPerson] = useState<Person>(); // ref type state
+	// const [friends2, setFriends2] = useState<Friend[]>([]);
+	const [friends, setFriends] = useState<Person['friends']>([]);
+	const [address, setAddress] = useState<Person['address']>({
+		city: 'Ankara',
+		country: 'TR',
+	});
+
+	const increase = () => {
+		setCounter(counter + 1);
+	};
+
+	// arrow function
+	const onChangePerson = () => {
+		setPerson({
+			firstName: 'ali',
+			lastName: 'can',
+			friends: [{ fullName: 'can', age: 21 }],
+		});
+
+		setFriends([
+			{
+				fullName: 'hakan',
+				age: 45,
+			},
+			{ fullName: 'mustafa', age: 34 },
+		]);
+
+		setAddress({ city: 'istanbul', country: 'TR' });
+	};
+
+	return (
+		<div>
+			<p>Sayac: {counter}</p>
+			<button onClick={increase}>(+)</button>
+			<p>
+				{person?.firstName} {person?.lastName}
+			</p>
+			<button onClick={onChangePerson}>Change Person</button>
+		</div>
+	);
 }
 
 export default useStateSample;
